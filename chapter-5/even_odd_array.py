c58503c5b7addf20c4747fc78a83b8f7cc93611c
Ehtesham <ehtesham599@gmail.com>
2022-03-25 22:46:46 +0300

    Solution to even_odd_array

---
+++ chapter-5/even_odd_array.py
@@ -0,0 +1,8 @@
+def even_odd(arr: List[int]) -> None:
+    next_even, next_odd = 0, len(arr) - 1
+
+    while next_even < next_odd:
+        if arr[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            arr[next_even], arr[next_odd] = arr[next_odd], arr[next_even]
